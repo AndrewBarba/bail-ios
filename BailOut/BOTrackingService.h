f6308ef75eeb299704ba723bf562ea65f3f3d21f
Andrew Barba <abarba.77@gmail.com>
2013-08-15 11:36:45 -0400

    cleanup

---
+++ BailOut/BOTrackingService.h
@@ -0,0 +1,15 @@
+//
+//  BOTrackingService.h
+//  BailOut
+//
+//  Created by Andrew Barba on 8/15/13.
+//  Copyright (c) 2013 Andrew Barba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BOTrackingService : NSObject
+
++ (instancetype)sharedTracker;
+
+@end
---
+++ BailOut/BOTrackingService.m
@@ -0,0 +1,48 @@
+//
+//  BOTrackingService.m
+//  BailOut
+//
+//  Created by Andrew Barba on 8/15/13.
+//  Copyright (c) 2013 Andrew Barba. All rights reserved.
+//
+
+#import "BOTrackingService.h"
+#import "GA/GAI.h"
+
+@implementation BOTrackingService
+
+/**
+ * Private initializer
+ */
+- (instancetype)_initPrivateTracker
+{
+    self = [super init];
+    if (self) {
+        // do setup here
+    }
+    return self;
+}
+
+/**
+ * Disable init, force static initializer
+ */
+- (id)init
+{
+    [super doesNotRecognizeSelector:_cmd];
+    return nil;
+}
+
+/*********** SHARED INSTANCE ************/
+/****************************************/
+
++ (instancetype)sharedTracker
+{
+    static BOTrackingService *sharedTracker = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedTracker = [[self alloc] _initPrivateTracker];
+    });
+    return sharedTracker;
+}
+
+@end
