b454e529cb516089159682228a76e0ec2264c8a1
Andrew Barba <abarba.77@gmail.com>
2013-08-15 11:24:59 -0400

    organization. add google analytics.

---
+++ BailOut/BOConstants.h
@@ -0,0 +1,14 @@
+//
+//  BOConstants.h
+//  BailOut
+//
+//  Created by Andrew Barba on 8/15/13.
+//  Copyright (c) 2013 Andrew Barba. All rights reserved.
+//
+
+#ifndef BailOut_BOConstants_h
+#define BailOut_BOConstants_h
+
+
+
+#endif
