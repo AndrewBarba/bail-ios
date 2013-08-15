04358f8cb500d0591c243ba45f0aafbc4da28e48
Andrew Barba <abarba.77@gmail.com>
2013-08-15 11:37:30 -0400

    added base view controller

---
+++ BailOut/BOViewController.m
@@ -0,0 +1,38 @@
+//
+//  BOViewController.m
+//  BailOut
+//
+//  Created by Andrew Barba on 8/15/13.
+//  Copyright (c) 2013 Andrew Barba. All rights reserved.
+//
+
+#import "BOViewController.h"
+
+@interface BOViewController ()
+
+@end
+
+@implementation BOViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
